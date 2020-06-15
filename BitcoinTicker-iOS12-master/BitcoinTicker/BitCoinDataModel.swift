16763e40321765a351a4665b07dacb14a83d1877
jervygu <jervygu@gmail.com>
2020-06-15 18:50:02 +0800

    Completed the module 38 ByteCoin App

---
+++ BitcoinTicker-iOS12-master/BitcoinTicker/BitCoinDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  BitCoinDataModel.swift
+//  BitcoinTicker
+//
+//  Created by Jaypee Umandap on 6/15/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class BitCoinDataModel {
+    var last : Double = 0.0
+}
+
